a9b04e1d5f6944dddb13675e1875cf5377773ef4
prnvbirajdar <prnvbirajdar@gmail.com>
2021-05-23 22:48:19 -0400

    form input added

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <h1>Todo List (Vue + TS)</h1>
+
+  <FormInput inputObj="inputData" />
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from "vue";
+import FormInput from "./components/FormInput.vue";
+
+export default defineComponent({
+  name: "App",
+  components: {
+    FormInput,
+  },
+  setup() {
+    const inputData = ref({
+      id: Date.now(),
+      title: "",
+      details: "",
+      completed: false
+    });
+
+    return { inputData };
+  },
+});
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/FormInput.vue
@@ -0,0 +1,30 @@
+<template>
+  <form action="">
+    <label for="title">Title:</label>
+    <input id="title" name="title" />
+
+    <label for="details">Details:</label>
+    <textarea name="details" id="details" cols="30" rows="10"></textarea>
+
+    <button type="submit">Submit</button>
+  </form>
+</template>
+
+<script lang="ts">
+import { ref, defineComponent } from "vue";
+export default defineComponent({
+  name: "HelloWorld",
+  props: {
+    inputObj: {
+      title: String,
+      details: String,
+      type: String,
+      required: true,
+    },
+  },
+  setup: () => {
+    const count = ref(0);
+    return { count };
+  },
+});
+</script>
